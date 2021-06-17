12f394347a1bd5a071d4810a17bb5fad29866012
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-06-16 23:10:58 -0700

    Add files via upload

---
+++ Harry/js/class3.js
@@ -0,0 +1,22 @@
+console.log('class3');
+
+
+var  name = "harry";
+
+console.log(name); 
+
+var city="dhaka";
+console.log(city);
+
+
+{
+
+    let city ="rampur";
+
+    console.log(city);
+}
+
+const arr1  = [12,45,16,2];
+
+arr1.push(45);
+console.log(arr1);
---
+++ Harry/js/index.js
@@ -0,0 +1,20 @@
+console.time('mycode');
+
+
+console.log('Hello');
+
+console.log(4+37);
+
+console.log([34,12,4515,4]);
+
+
+console.table({roy:'zit',marks:50});
+
+
+console.warn('prosehnzit');
+
+// console.clear();
+
+console.timeEnd('mycode');
+
+console.assert(554<154 ,'age> is not possible');
